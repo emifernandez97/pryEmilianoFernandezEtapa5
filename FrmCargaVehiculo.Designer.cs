56aca38dcdfa14ee4dc14b9d1061686e2a1f0d44
Emiliano Fernandez <emifernandez_97@hotmail.com>
2024-05-12 23:41:19 -0300

    Agregar archivos de proyecto.

---
+++ FrmCargaVehiculo.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace pryEmilianoFernandezEtapa2
+{
+    partial class FrmCargaVehiculo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnCargar = new Button();
+            PctVehiculo = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)PctVehiculo).BeginInit();
+            SuspendLayout();
+            // 
+            // BtnCargar
+            // 
+            BtnCargar.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnCargar.Location = new Point(308, 382);
+            BtnCargar.Name = "BtnCargar";
+            BtnCargar.Size = new Size(187, 70);
+            BtnCargar.TabIndex = 0;
+            BtnCargar.Text = "Cargar";
+            BtnCargar.UseVisualStyleBackColor = true;
+            BtnCargar.Click += BtnCargar_Click;
+            // 
+            // PctVehiculo
+            // 
+            PctVehiculo.BackColor = Color.White;
+            PctVehiculo.BorderStyle = BorderStyle.Fixed3D;
+            PctVehiculo.Location = new Point(12, 12);
+            PctVehiculo.Name = "PctVehiculo";
+            PctVehiculo.Size = new Size(785, 364);
+            PctVehiculo.SizeMode = PictureBoxSizeMode.StretchImage;
+            PctVehiculo.TabIndex = 1;
+            PctVehiculo.TabStop = false;
+            // 
+            // FrmCargaVehiculo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(809, 464);
+            Controls.Add(PctVehiculo);
+            Controls.Add(BtnCargar);
+            Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "FrmCargaVehiculo";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Carga de Vehiculo";
+            ((System.ComponentModel.ISupportInitialize)PctVehiculo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnCargar;
+        private PictureBox PctVehiculo;
+    }
+}
---
+++ clsVehiculo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEmilianoFernandezEtapa2
+{
+    internal class clsVehiculo
+    {
+        //Propiedades
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public Image Imagen { get; set; }
+
+        //Constructor
+        /*public clsVehiculo(string nombre, string tipo, Image imagen)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+            Imagen = imagen;
+        }*/
+
+        //Metodos
+
+        public void crearVehiculo(PictureBox Vehiculo) //pct del formulario
+        {
+            PictureBox ImagenVehiculo = new PictureBox(); //instancio en memora imagen
+
+            Random rnd = new Random(); //creo random
+
+            // Generar una posición aleatoria dentro del PictureBox contenedor
+            int posX = rnd.Next(0, Vehiculo.Width - 50);
+            int posY = rnd.Next(0, Vehiculo.Height - 100);
+
+            switch (rnd.Next(1,4))  //para cada caso un random
+            {
+                case 1:
+                    Imagen = pryEmilianoFernandezEtapa5.Properties.Resources.Rayo_Mckuin;
+                    break;
+                case 2:
+                    Imagen = pryEmilianoFernandezEtapa5.Properties.Resources.CAPTAIN_removebg_preview;
+                    break;
+                case 3:
+                    Imagen = pryEmilianoFernandezEtapa5.Properties.Resources.jay_jay_removebg_preview;
+                    break;
+            }
+
+            ImagenVehiculo.Image = Imagen;
+            ImagenVehiculo.SizeMode = PictureBoxSizeMode.StretchImage;
+            ImagenVehiculo.Location = new Point (posX,posY);
+            ImagenVehiculo.Size = new Size(100 , 100);
+            ImagenVehiculo.BackColor = Color.White;
+            Vehiculo.Controls.Add(ImagenVehiculo);
+
+            ImagenVehiculo.BringToFront();          
+        }
+    }
+}
